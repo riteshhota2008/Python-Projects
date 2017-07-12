1fb9373919d6d984dc6a423fca6dfcf41f3ee9b9
Ritesh Hota <riteshhota.2008@gmail.com>
2017-07-12 21:27:15 +0530

    Mortgage Calculator

--- mortgage-calculator.py
+++ mortgage-calculator.py
@@ -1,4 +1,5 @@
 """**Mortgage Calculator** -
+
 Calculate the monthly payments of a fixed term mortgage
 over given Nth terms at a given interest rate. Also figure
 out how long it will take the user to pay back the loan."""
