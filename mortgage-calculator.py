6b233aa56dfa191a75f7c381b9c832100f28ea7f
Ritesh Hota <riteshhota.2008@gmail.com>
2017-07-12 21:20:32 +0530

    Initial Commit

---
+++ mortgage-calculator.py
@@ -0,0 +1,13 @@
+"""**Mortgage Calculator** -
+Calculate the monthly payments of a fixed term mortgage
+over given Nth terms at a given interest rate. Also figure
+out how long it will take the user to pay back the loan."""
+
+months = int(input("Enter mortgage term (in months): "))
+rate = float(input("Enter interest rate: "))
+loan = float(input("Enter loan value: "))
+
+monthly_rate = rate / 100 / 12
+payment = (monthly_rate / (1 - (1 + monthly_rate) ** (-months))) * loan
+
+print ("Monthly payment for a Rs. %.2f %s year mortgage at %.2f%% interest rate is: Rs. %.2f" % (loan, (months / 12), rate, payment))
